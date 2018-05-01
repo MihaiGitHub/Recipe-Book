511f8e7a7a24f6bb4882db3d99cacb7bcc814df8
Mihai Smarandache <mihailiviu@yahoo.com>
2018-05-01 12:07:39 -0700

    Add ingredients array to Recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from '@angular/core';
 
 // Import Recipe model to use in recipe service
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 // Service to manage our recipes
 export class RecipeService {
@@ -10,8 +11,25 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>(); // It will hold recipe data
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
-        new Recipe('Another Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+        // Need to pass in an array of ingredients for each Recipe now
+        new Recipe(
+            'Tasty Schnitzel', 
+            'This is simply a test', 
+            'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big Fat Burger', 
+            'This is simply a test', 
+            'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
+        )
       ];
 
     getRecipes(){
