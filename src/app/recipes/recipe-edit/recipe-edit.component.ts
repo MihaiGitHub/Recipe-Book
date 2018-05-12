b4e639ba261745f48853f3de68accf52b74d1606
Mihai Smarandache <mihailiviu@yahoo.com>
2018-05-12 13:31:25 -0700

    Initialize add recipe form empty if new recipe or in edit mode if an existing recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,8 +12,9 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   // Need to differentiate wheather editing recipe or creating a new one
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -19,8 +22,30 @@ export class RecipeEditComponent implements OnInit {
           this.id = +params['id'];
           // Determine edit/new recipe if ID is defined
           this.editMode = params['id'] != null;
+          // Call this method every time the route params change which = page reload
+          this.initForm();
         }
     );
   }
 
+  // Initializing a reactive form; either empty if new recipe or in edit mode with existing recipe
+  private initForm(){
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    // if in edit mode grab the recipe being edited using recipe service
+    if(this.editMode){
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
+    });
+  }
 }
