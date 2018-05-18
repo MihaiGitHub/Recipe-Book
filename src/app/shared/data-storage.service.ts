a39eed78a59b4a1725bea9e12ebe95be8428365f
Mihai Smarandache <mihailiviu@yahoo.com>
2018-05-17 21:32:35 -0700

    Send the unique Token in requests to server using Firebase SDK

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,19 +4,26 @@ import 'rxjs/add/operator/map';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService){}
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService){}
 
     storeRecipes(){ // PUT recipes on the server
-       return this.http.put('https://ng-recipe-book-4bb73.firebaseio.com/recipes.json', 
+       const token = this.authService.getToken();
+
+       return this.http.put('https://ng-recipe-book-4bb73.firebaseio.com/recipes.json?auth='+token, 
             this.recipeService.getRecipes()
         );
     }
 
     getRecipes(){
-        this.http.get('https://ng-recipe-book-4bb73.firebaseio.com/recipes.json')
+        // When getting recipes need to attach unique token
+        const token = this.authService.getToken();
+
+        // Firebase will recognize the auth query parameter
+        this.http.get('https://ng-recipe-book-4bb73.firebaseio.com/recipes.json?auth='+token)
             .map(
                 (response: Response) => {
                     const recipes : Recipe[] = response.json();
