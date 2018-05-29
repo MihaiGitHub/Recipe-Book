1ae086cf7f4bc4aad3dc04b9587ba0225c967482
Mihai Smarandache <mihailiviu@yahoo.com>
2018-05-28 21:45:25 -0700

    Add Lazy Loading to Recipes Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+    { path: '', component: HomeComponent },
+    // Lazy Loading of RecipesModule only when /recipes is entered in the URL
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }, // need to specify name of class to load RecipesModule
     { path: 'shopping-list', component: ShoppingListComponent }
 ]
 
